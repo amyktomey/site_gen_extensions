61500966e1ac618174528f1df8c96a485404cdab
Amy Tomey <amy.tomey59@gmail.com>
2021-11-05 23:18:46 -0400

    coding complete, with a few import errors I am tracking dowm, as well as possible missing commas

--- pluralsight-projects-python-extensions-static-site-generator-f0fb050/ssg/extensions/stats.py
+++ pluralsight-projects-python-extensions-static-site-generator-f0fb050/ssg/extensions/stats.py
@@ -4,20 +4,21 @@ import time
 start_time = None
 total_written = 0
 
-@"start_build"()
+@'start_build'()
 def start_build():
     global start_time
     start_time = time
 
-@"written"()
+@'written'()
 def written():
     global total_written
     total_written = total_written + 1
 
-@"stats"()
+@'stats'()
 def stats():
     final_time = time - start_time
-    average = final_time / total_written if total_written > 0 else None
-    report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
+    average = (final_time / total_written) if total_written > 0 else None
+    
+report = ("Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file")
 
 print (report.format('total_written', 'final_time', 'average'))
