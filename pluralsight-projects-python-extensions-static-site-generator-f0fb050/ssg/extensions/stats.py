19d23fdba467bf802bad5a84d1a17d388e6cbb09
Amy Tomey <amy.tomey59@gmail.com>
2021-11-07 17:45:58 -0500

    corrected decorator in stats.py and cleaned up imports

--- pluralsight-projects-python-extensions-static-site-generator-f0fb050/ssg/extensions/stats.py
+++ pluralsight-projects-python-extensions-static-site-generator-f0fb050/ssg/extensions/stats.py
@@ -1,20 +1,20 @@
-from ssg import hooks
+import hooks
 import time
 
 start_time = None
 total_written = 0
 
-@'start_build'()
+@hooks.register('start_build')
 def start_build():
     global start_time
     start_time = time
 
-@'written'()
+@hooks.register('written')
 def written():
     global total_written
     total_written = total_written + 1
 
-@'stats'()
+@hooks.register('stats')
 def stats():
     final_time = time - start_time
     average = (final_time / total_written) if total_written > 0 else None
